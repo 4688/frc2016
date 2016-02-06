fb98df530fd9094b4592f5df4dbdf9245e8a507f
jacob c <jacobcazabon@gmail.com>
2016-02-06 09:32:39 -0500

    More comments & double motor template code

--- robot.py
+++ robot.py
@@ -10,9 +10,17 @@ class SweetAssRobot(wpi.IterativeRobot):
         self.lMotor0.changeControlMode(wpi.CANTalon.ControlMode.PercentVbus)
         self.lMotor0.set(0.0)
 
+        # self.lMotor1 = wpi.CANTalon(L_MOTOR_INDICES[1])
+        # self.lMotor1.changeControlMode(wpi.CANTalon.ControlMode.PercentVbus)
+        # self.lMotor1.set(0.0)
+
         self.rMotor0 = wpi.CANTalon(R_MOTOR_INDICES[0])
         self.rMotor0.changeControlMode(wpi.CANTalon.ControlMode.PercentVbus)
         self.rMotor0.set(0.0)
+
+        # self.rMotor1 = wpi.CANTalon(R_MOTOR_INDICES[1])
+        # self.rMotor1.changeControlMode(wpi.CANTalon.ControlMode.PercentVbus)
+        # self.rMotor1.set(0.0)
 
         self.controls = wpi.Joystick(JOYSTICK_INDEX)
 
@@ -23,12 +31,15 @@ class SweetAssRobot(wpi.IterativeRobot):
         print("  > Entered MANUAL mode.")
 
     def teleopPeriodic(self):
+        # Control stick axes
         yAxis = self.controls.getRawAxis(FORWARD_AXIS_INDEX)
         xAxis = self.controls.getRawAxis(TURN_AXIS_INDEX)
 
+        # Turbo stuff
         isTurbo = self.controls.getRawButton(TURBO_BUTTON_INDEX) and ALLOW_TURBO
         turboMultiplier = TURBO_MULT if isTurbo else 1
 
+        # Amount to move forward
         forward = -yAxis / FORWARD_DIVISOR * turboMultiplier
         leftSpeed = forward
         rightSpeed = -forward
@@ -40,15 +51,15 @@ class SweetAssRobot(wpi.IterativeRobot):
             rightSpeed = turnSpeed
         elif forward > MOTOR_DEADBAND or forward < -MOTOR_DEADBAND:
             # Turning whilst driving
-            poorlyNamedVariable = (abs(xAxis) + 1)
+            turnFactor = (abs(xAxis) + 1)
             if xAxis > MOTOR_DEADBAND:
                 # X axis is positive (to the right)
-                leftSpeed *= poorlyNamedVariable
-                rightSpeed /= poorlyNamedVariable
+                leftSpeed *= turnFactor
+                rightSpeed /= turnFactor
             elif xAxis < -MOTOR_DEADBAND:
                 # X axis if negative (to the left)
-                leftSpeed /= poorlyNamedVariable
-                rightSpeed *= poorlyNamedVariable
+                leftSpeed /= turnFactor
+                rightSpeed *= turnFactor
 
         self.lMotor0.set(leftSpeed)
         self.rMotor0.set(rightSpeed)
