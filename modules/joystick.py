c2c9ed7caee6fbd2ff58f6a47afa33db131cdd11
jacob c <jacobcazabon@gmail.com>
2016-02-15 15:08:28 -0500

    Slam bam thank you ma'am

---
+++ modules/joystick.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+# CONSTANTS
+#===============================================================================
+
+# USB device port index of the joystick.
+# Default: 0 (first port)
+JOYSTICK_INDEX = 0
+
+# AXES
+#-------------------------------------------------------------------------------
+
+# Joystick axis index for forward/backward drive control.
+# Default: 1 (Analog stick inverted Y-axis)
+Y_AXIS_INDEX = 1
+
+# Joystick axis index for left/right drive control.
+# Default: 3 (C-stick X-axis)
+X_AXIS_INDEX = 3
+
+# Joystick axis index for ball intake motors.
+# Default: 2 (L button pressure)
+INTAKE_AXIS_INDEX = 2
+
+# BUTTONS
+#-------------------------------------------------------------------------------
+
+# Joystick button index for turbo mode.
+# Default: 5 (Z button)
+TURBO_BTN_INDEX = 5
+
+# Joystick button index for ball ejection command.
+# Default: 1 (A button)
+EJECT_BTN_INDEX = 1
+
+# Joystick button index for boom extension.
+# Default: 3 (X button)
+EXTEND_BTN_INDEX = 3
+
+# Joystick button index for boom retraction.
+# Default: 4 (Y button)
+RETRACT_BTN_INDEX = 4
---
+++ robot.py
@@ -0,0 +1,81 @@
+#!/usr/bin/python3
+
+import modules.arm as a
+import modules.ball as b
+import modules.drive as d
+import modules.joystick as j
+import modules.station as s
+
+import wpilib as wpi
+
+class SweetAssRobot(wpi.IterativeRobot):
+
+    def robotInit(self):
+        """
+            Called once when the robot starts up.
+            This is where we initialize all of the robot's components.
+        """
+
+        # Joystick
+        self.joystick = wpi.Joystick(j.JOYSTICK_INDEX)
+
+        # Left front drive motor
+        self.lMotor0 = wpi.CANTalon(d.L_CAN_INDICES[0])
+        self.lMotor0.changeControlMode(wpi.CANTalon.ControlMode.PercentVbus)
+        self.lMotor0.set(0.0)
+
+        # Left back drive motor
+        self.lMotor1 = wpi.CANTalon(d.L_CAN_INDICES[1])
+        self.lMotor1.changeControlMode(wpi.CANTalon.ControlMode.PercentVbus)
+        self.lMotor1.set(0.0)
+
+        # Right front drive motor
+        self.rMotor0 = wpi.CANTalon(d.R_CAN_INDICES[0])
+        self.rMotor0.changeControlMode(wpi.CANTalon.ControlMode.PercentVbus)
+        self.rMotor0.set(0.0)
+
+        # Right back drive motor
+        self.rMotor1 = wpi.CANTalon(d.R_CAN_INDICES[1])
+        self.rMotor1.changeControlMode(wpi.CANTalon.ControlMode.PercentVbus)
+        self.rMotor1.set(0.0)
+
+        # Left intake motor
+        self.lIntakeMotor = wpi.VictorSP(b.L_INTAKE_INDEX)
+        self.lIntakeMotor.set(0.0)
+
+        # Right intake motor
+        self.rIntakeMotor = wpi.VictorSP(b.R_INTAKE_INDEX)
+        self.rIntakeMotor.set(0.0)
+
+        # Initialize camera stream
+        s.startCamera()
+
+    def teleopPeriodic(self):
+        """
+            Called 50 times per second while the robot is in teleop (manual)
+            mode. This is essentially one "frame" of the robot code; i.e. this
+            is where we interpret user input, process built-in logic, and send
+            component commands, in that order.
+        """
+
+        lDriveSpd = d.getDriveLeft(joystick=self.joystick)
+        self.lMotor0.set(lDriveSpd)
+        self.lMotor1.set(lDriveSpd)
+
+        rDriveSpd = d.getDriveRight(joystick=self.joystick)
+        self.rMotor0.set(rDriveSpd)
+        self.rMotor1.set(rDriveSpd)
+
+        intakeSpd = b.getIntakeSpeed(joystick=self.joystick)
+        self.lIntakeMotor.set(-intakeSpd)
+        self.rIntakeMotor.set(intakeSpd)
+
+    def testPeriodic(self):
+        """
+            Called 50 times per second while the robot is in test mode.
+        """
+
+        wpi.LiveWindow.run()
+
+if __name__ == "__main__":
+    wpi.run(SweetAssRobot)
